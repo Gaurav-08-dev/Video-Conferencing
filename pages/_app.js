b7c68b330d0522ee67f6412d35f23c276e4e4856
gaurav-08-dev <g8111997@gmail.com>
2024-01-01 22:14:09 +0530

    Setup socket.io in client and server - Done

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
+import { SocketProvider } from '@/context/socket.js';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SocketProvider>
+      <Component {...pageProps} />
+    </SocketProvider>
+  )
 }
--- pages/api/socket.js
+++ pages/api/socket.js
@@ -3,16 +3,19 @@ import { Server } from "socket.io";
 const SocketHandler = (req, res) => {
 
     if (res.socket.server.io) {
-        console.log("Socket is already running")
+
+        console.log("Socket is already running");
+
     }
     else {
 
         const io = new Server(res.socket.server);
         res.socket.server.io = io;
 
-        io.on('connectoin', (socket) => {
-            console.log("Server is connected")
+        io.on('connection', (socket) => {
+            console.log("Server is connected");
         })
+
     }
     res.end();
 }
